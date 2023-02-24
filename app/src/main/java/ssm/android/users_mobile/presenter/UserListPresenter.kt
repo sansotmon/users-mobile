833acdaffb6c9b79ceb79241e0da7b945aecbf64
Santiago Soto <sansotmon@gmail.com>
2023-02-24 01:18:21 -0500

    added network permission in manifest

--- app/src/main/java/ssm/android/users_mobile/presenter/UserListPresenter.kt
+++ app/src/main/java/ssm/android/users_mobile/presenter/UserListPresenter.kt
@@ -2,6 +2,7 @@ package ssm.android.users_mobile.presenter
 
 import android.content.Context
 import com.google.gson.Gson
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import ssm.android.users_mobile.R
@@ -12,10 +13,10 @@ class UserListPresenter(val context: Context, val ui: UserListUI) : BasePresente
     private var users: List<User> = arrayListOf()
 
     fun getUsers(){
-        ui.showProgressDialog(context.getString(R.string.activity_user_list_loading))
+        ui.showToast(context.getString(R.string.activity_user_list_loading))
         okInteractor.getUsers({responseBody ->
-            val json: JsonObject = JsonParser().parse(responseBody).asJsonObject
-            if(json.get("error") == null){
+            val json: JsonArray = JsonParser().parse(responseBody).asJsonArray
+            if(json.get(1) == null){
                 val response = Gson().fromJson(responseBody, User.Users::class.java)
                 users = response.users!!
                 users.forEach { user ->
@@ -23,7 +24,7 @@ class UserListPresenter(val context: Context, val ui: UserListUI) : BasePresente
                     ui.showToast(obj.id)
                 }
             }else{
-                ui.showMessageDialog(json.get("ERROR").toString())
+                ui.showMessageDialog(json.get(1).toString())
             }
         }, {error->
             error?.let {
