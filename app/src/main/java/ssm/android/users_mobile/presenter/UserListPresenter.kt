154a12445d4088802aa994c9ff47efd123e1bf1e
Santiago Soto <sansotmon@gmail.com>
2023-02-25 00:30:13 -0500

    added db validation to show user from database

--- app/src/main/java/ssm/android/users_mobile/presenter/UserListPresenter.kt
+++ app/src/main/java/ssm/android/users_mobile/presenter/UserListPresenter.kt
@@ -2,6 +2,7 @@ package ssm.android.users_mobile.presenter
 
 import android.content.ContentValues
 import android.content.Context
+import android.provider.BaseColumns
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 import com.google.gson.JsonParser
@@ -17,23 +18,64 @@ class UserListPresenter(private val context: Context, private val ui: UserListUI
     private val dbHelper = UserSQLiteHelper(context)
 
     fun getUsers(){
-        ui.showToast(context.getString(R.string.activity_user_list_loading))
-        okInteractor.getUsers({responseBody ->
-            val userListDict: JsonArray = JsonParser().parse(responseBody).asJsonArray
-            for (userDict in userListDict){
-                val user = Gson().fromJson(userDict, User::class.java)
+        if(!getUsersFromDB()){
+            ui.showToast(context.getString(R.string.activity_user_list_loading))
+            okInteractor.getUsers({responseBody ->
+                val userListDict: JsonArray = JsonParser().parse(responseBody).asJsonArray
+                for (userDict in userListDict){
+                    val user = Gson().fromJson(userDict, User::class.java)
+                    users.add(user)
+                    insertUser(user)
+                }
+                if(users.isNotEmpty()){
+                    tempUsers = users
+                    ui.refreshRecycler()
+                }
+            }, {error->
+                error?.let {
+                    ui.showMessageDialog(it)
+                }
+            })
+        }
+    }
+
+    private fun getUsersFromDB():Boolean{
+        val db = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            BaseColumns._ID,
+            UserContract.UserEntry.COLUMN_NAME_NAME,
+            UserContract.UserEntry.COLUMN_NAME_EMAIL,
+            UserContract.UserEntry.COLUMN_NAME_PHONE
+        )
+
+        val cursor = db.query(
+            UserContract.UserEntry.TABLE_NAME,
+            projection,
+            null,
+            null,
+            null,
+            null,
+            null,
+        )
+
+        with(cursor) {
+            while (moveToNext()) {
+                val id = getString(getColumnIndexOrThrow(BaseColumns._ID))
+                val name = getString(getColumnIndexOrThrow(UserContract.UserEntry.COLUMN_NAME_NAME))
+                val email = getString(getColumnIndexOrThrow(UserContract.UserEntry.COLUMN_NAME_EMAIL))
+                val phone = getString(getColumnIndexOrThrow(UserContract.UserEntry.COLUMN_NAME_PHONE))
+                val user = User(id,name,null,email,phone)
                 users.add(user)
-                insertUser(user)
             }
             if(users.isNotEmpty()){
                 tempUsers = users
                 ui.refreshRecycler()
             }
-        }, {error->
-            error?.let {
-                ui.showMessageDialog(it)
-            }
-        })
+        }
+        cursor.close()
+
+        return tempUsers.isNotEmpty()
     }
 
     private fun insertUser(user: User){
